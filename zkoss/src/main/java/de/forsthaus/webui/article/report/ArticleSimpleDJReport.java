1d42047b1c32d9210b6812a4e5f7de2d8dab6431
terrytornado <terrytornado@44dfe7d0-046a-4be5-b101-54f5111bae67>
2010-08-09 20:24:49 +0000

    Added the DynamicJasper libs. First Report based on this framework. Great :-)

---
+++ zkoss/src/main/java/de/forsthaus/webui/article/report/ArticleSimpleDJReport.java
@@ -0,0 +1,274 @@
+/**
+ * Copyright 2010 the original author or authors.
+ * 
+ * This file is part of Zksample2. http://zksample2.sourceforge.net/
+ *
+ * Zksample2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Zksample2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Zksample2.  If not, see <http://www.gnu.org/licenses/gpl.html>.
+ */
+package de.forsthaus.webui.article.report;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JExcelApiExporter;
+import net.sf.jasperreports.engine.export.JExcelApiExporterParameter;
+import net.sf.jasperreports.engine.export.JRRtfExporter;
+
+import org.zkoss.spring.SpringUtil;
+import org.zkoss.util.media.AMedia;
+import org.zkoss.util.resource.Labels;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.SuspendNotAllowedException;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zul.Iframe;
+import org.zkoss.zul.Window;
+
+import ar.com.fdvs.dj.core.DynamicJasperHelper;
+import ar.com.fdvs.dj.core.layout.ClassicLayoutManager;
+import ar.com.fdvs.dj.domain.DynamicReport;
+import ar.com.fdvs.dj.domain.Style;
+import ar.com.fdvs.dj.domain.builders.ColumnBuilderException;
+import ar.com.fdvs.dj.domain.builders.FastReportBuilder;
+import ar.com.fdvs.dj.domain.constants.Border;
+import ar.com.fdvs.dj.domain.constants.Font;
+import ar.com.fdvs.dj.domain.constants.HorizontalAlign;
+import de.forsthaus.backend.model.Article;
+import de.forsthaus.backend.service.ArticleService;
+import de.forsthaus.webui.util.FDDateFormat;
+import de.forsthaus.webui.util.FDUtils;
+
+/**
+ * A simple article report implemented with the DynamicJasper framework.<br>
+ * <br>
+ * This Report uses the FastReportBuilder that have many parameters defined as
+ * defaults, so it's very easy to create a simple report with it.<br>
+ * 
+ * @author bbruhns
+ * @author sge
+ * 
+ */
+public class ArticleSimpleDJReport extends Window implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Window window;
+	private Iframe iFrame;
+	private ByteArrayOutputStream output;
+	private InputStream mediais;
+	private AMedia amedia;
+	private String zksample2title = "[Zksample2] DynamicJasper Report Sample";
+
+	public ArticleSimpleDJReport(Component parent) throws InterruptedException {
+		super();
+		this.setParent(parent);
+
+		try {
+			doPrint();
+		} catch (Exception e) {
+			FDUtils.showErrorMessage(e.toString());
+		}
+	}
+
+	public void doPrint() throws JRException, ColumnBuilderException, ClassNotFoundException, IOException {
+
+		FastReportBuilder drb = new FastReportBuilder();
+		DynamicReport dr;
+
+		/**
+		 * Set the styles. In a report created with DynamicReportBuilder we do
+		 * this in an other way.
+		 */
+		// Rows content
+		Style columnStyleNumbers = new Style();
+		columnStyleNumbers.setFont(Font.ARIAL_SMALL);
+		columnStyleNumbers.setHorizontalAlign(HorizontalAlign.RIGHT);
+
+		// Header for number row content
+		Style columnStyleNumbersBold = new Style();
+		columnStyleNumbersBold.setFont(Font.ARIAL_MEDIUM_BOLD);
+		columnStyleNumbersBold.setHorizontalAlign(HorizontalAlign.RIGHT);
+		columnStyleNumbersBold.setBorderBottom(Border.PEN_1_POINT);
+
+		// Rows content
+		Style columnStyleText = new Style();
+		columnStyleText.setFont(Font.ARIAL_SMALL);
+		columnStyleText.setHorizontalAlign(HorizontalAlign.LEFT);
+
+		// Header for String row content
+		Style columnStyleTextBold = new Style();
+		columnStyleTextBold.setFont(Font.ARIAL_MEDIUM_BOLD);
+		columnStyleTextBold.setHorizontalAlign(HorizontalAlign.LEFT);
+		columnStyleTextBold.setBorderBottom(Border.PEN_1_POINT);
+
+		// Subtitle
+		Style subtitleStyle = new Style();
+		subtitleStyle.setHorizontalAlign(HorizontalAlign.LEFT);
+		subtitleStyle.setFont(Font.ARIAL_MEDIUM_BOLD);
+
+		String artNo = Labels.getLabel("common.Article.No");
+		String artShortText = Labels.getLabel("common.Description.Short");
+		String artPrice = Labels.getLabel("common.Price");
+
+		drb.addColumn(artNo, "artNr", String.class.getName(), 20, columnStyleText, columnStyleTextBold);
+		drb.addColumn(artShortText, "artKurzbezeichnung", String.class.getName(), 50, columnStyleText, columnStyleTextBold);
+		drb.addColumn(artPrice, "artPreis", String.class.getName(), 20, columnStyleNumbers, columnStyleNumbersBold);
+
+		// Sets the Report Columns, header, Title, Groups, Etc Formats
+		// DynamicJasper documentation
+		drb.setTitle(zksample2title);
+		drb.setSubtitle("Article-List: " + FDDateFormat.getDateFormater().format(new Date()));
+		drb.setSubtitleStyle(subtitleStyle);
+		drb.setPrintBackgroundOnOddRows(true);
+		drb.setUseFullPageWidth(true);
+		dr = drb.build();
+
+		// Get information from database
+		ArticleService as = (ArticleService) SpringUtil.getBean("articleService");
+		List<Article> articles = as.getAllArticles();
+
+		// Create Datasource and put it in Dynamic Jasper Format
+		List data = new ArrayList(articles.size());
+
+		for (Article article : articles) {
+			Map<String, String> map1 = new HashMap<String, String>();
+			map1.put("artNr", article.getArtNr());
+			map1.put("artKurzbezeichnung", article.getArtKurzbezeichnung());
+			map1.put("artPreis", String.valueOf(article.getArtPreis()));
+			data.add(map1);
+		}
+
+		// Generate the Jasper Print Object
+		JRDataSource ds = new JRBeanCollectionDataSource(data);
+		JasperPrint jp = DynamicJasperHelper.generateJasperPrint(dr, new ClassicLayoutManager(), ds);
+
+		String outputFormat = "PDF";
+
+		output = new ByteArrayOutputStream();
+
+		if (outputFormat.equalsIgnoreCase("PDF")) {
+			JasperExportManager.exportReportToPdfStream(jp, output);
+			mediais = new ByteArrayInputStream(output.toByteArray());
+			amedia = new AMedia("FirstReport.pdf", "pdf", "application/pdf", mediais);
+
+			callReportWindow(amedia, "PDF");
+		} else if (outputFormat.equalsIgnoreCase("XLS")) {
+			JExcelApiExporter exporterXLS = new JExcelApiExporter();
+			exporterXLS.setParameter(JExcelApiExporterParameter.JASPER_PRINT, jp);
+			exporterXLS.setParameter(JExcelApiExporterParameter.OUTPUT_STREAM, output);
+			exporterXLS.setParameter(JExcelApiExporterParameter.IS_DETECT_CELL_TYPE, Boolean.TRUE);
+			exporterXLS.setParameter(JExcelApiExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.TRUE);
+			exporterXLS.setParameter(JExcelApiExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE);
+			exporterXLS.exportReport();
+			mediais = new ByteArrayInputStream(output.toByteArray());
+			amedia = new AMedia("FileFormatExcel", "xls", "application/vnd.ms-excel", mediais);
+
+			callReportWindow(amedia, "XLS");
+		} else if (outputFormat.equalsIgnoreCase("RTF") || outputFormat.equalsIgnoreCase("DOC")) {
+			JRRtfExporter exporterRTF = new JRRtfExporter();
+			exporterRTF.setParameter(JRExporterParameter.JASPER_PRINT, jp);
+			exporterRTF.setParameter(JRExporterParameter.OUTPUT_STREAM, output);
+			exporterRTF.exportReport();
+			mediais = new ByteArrayInputStream(output.toByteArray());
+			amedia = new AMedia("FileFormatRTF", "rtf", "application/rtf", mediais);
+
+			callReportWindow(amedia, "RTF-DOC");
+		}
+	}
+
+	private void callReportWindow(AMedia aMedia, String format) {
+		boolean modal = true;
+
+		this.setTitle("Dynamic JasperReports. Sample Report for ZKoss");
+		this.setId("ReportWindow");
+		this.setVisible(true);
+		this.setMaximizable(true);
+		this.setMinimizable(true);
+		this.setSizable(true);
+		this.setClosable(true);
+		this.setHeight("100%");
+		this.setWidth("80%");
+		this.addEventListener("onClose", new OnCloseReportEventListener());
+
+		iFrame = new Iframe();
+		iFrame.setId("jasperReportId");
+		iFrame.setWidth("100%");
+		iFrame.setHeight("100%");
+		iFrame.setContent(aMedia);
+		iFrame.setParent(this);
+
+		if (modal == true) {
+			try {
+				this.doModal();
+			} catch (SuspendNotAllowedException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	/**
+	 * EventListener for closing the Report Window.<br>
+	 * 
+	 * @author sge
+	 * 
+	 */
+	public final class OnCloseReportEventListener implements EventListener {
+		@Override
+		public void onEvent(Event event) throws Exception {
+			closeReportWindow();
+		}
+	}
+
+	/**
+	 * We must clear something to prevent errors or problems <br>
+	 * by opening the report a few times. <br>
+	 * 
+	 * @throws IOException
+	 */
+	private void closeReportWindow() throws IOException {
+
+		this.removeEventListener("onClose", new OnCloseReportEventListener());
+
+		// TODO check this
+		try {
+			amedia.getStreamData().close();
+			output.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+		this.onClose();
+
+	}
+
+}

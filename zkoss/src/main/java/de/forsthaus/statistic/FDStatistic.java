bef9c077e00a2ba590392a2860b53c5c2f66e9e6
bbruhns <bbruhns@44dfe7d0-046a-4be5-b101-54f5111bae67>
2010-08-19 20:44:52 +0000

    new FDStatistic

---
+++ zkoss/src/main/java/de/forsthaus/statistic/FDStatistic.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright 2010 the original author or authors.
+ * 
+ * This file is part of Zksample2. http://zksample2.sourceforge.net/
+ *
+ * Zksample2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Zksample2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Zksample2.  If not, see <http://www.gnu.org/licenses/gpl.html>.
+ */
+package de.forsthaus.statistic;
+
+/**
+ * @author bbruhns
+ * 
+ */
+public final class FDStatistic {
+
+	public static FDStatistic getStatistic() {
+		return FDStatisticMonitor.getStatistic();
+	}
+
+	FDStatistic(long startTime, int activeDesktopCount, int totalSessionCount, int activeSessionCount,
+			int totalDesktopCount, int totalUpdateCount, int activeUpdateCount) {
+		super();
+		this.startTime = startTime;
+		this.activeDesktopCount = activeDesktopCount;
+		this.totalSessionCount = totalSessionCount;
+		this.activeSessionCount = activeSessionCount;
+		this.totalDesktopCount = totalDesktopCount;
+		this.totalUpdateCount = totalUpdateCount;
+		this.activeUpdateCount = activeUpdateCount;
+	}
+
+	private final long currentTime = System.currentTimeMillis();
+	private final long startTime;
+	private final int activeDesktopCount;
+	private final int totalSessionCount;
+	private final int activeSessionCount;
+	private final int totalDesktopCount;
+	private final int totalUpdateCount;
+	private final int activeUpdateCount;
+
+	public int getActiveDesktopCount() {
+		return activeDesktopCount;
+	}
+
+	public int getActiveSessionCount() {
+		return activeSessionCount;
+	}
+
+	public double getActiveUpdateCount() {
+		return activeUpdateCount;
+	}
+
+	public double getAverageDesktopCount() {
+		return this.activeDesktopCount / getRuningHours();
+	}
+
+	public double getAverageSessionCount() {
+		return this.totalSessionCount / getRuningHours();
+	}
+
+	public double getAverageUpdateCount() {
+		return this.totalUpdateCount / getRuningHours();
+	}
+
+	public double getRuningHours() {
+		final long v = this.currentTime - this.startTime;
+		return v / 3600000d;
+	}
+
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public int getTotalDesktopCount() {
+		return totalDesktopCount;
+	}
+
+	public int getTotalSessionCount() {
+		return totalSessionCount;
+	}
+
+	public int getTotalUpdateCount() {
+		return totalUpdateCount;
+	}
+}
---
+++ zkoss/src/main/java/de/forsthaus/statistic/FDStatisticMonitor.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright 2010 the original author or authors.
+ * 
+ * This file is part of Zksample2. http://zksample2.sourceforge.net/
+ *
+ * Zksample2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Zksample2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Zksample2.  If not, see <http://www.gnu.org/licenses/gpl.html>.
+ */
+package de.forsthaus.statistic;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.zkoss.zk.ui.Desktop;
+import org.zkoss.zk.ui.Session;
+import org.zkoss.zk.ui.util.Monitor;
+
+/**
+ * @author bbruhns
+ * 
+ */
+public final class FDStatisticMonitor implements Monitor, Serializable {
+	private static final long serialVersionUID = -4973184924311461356L;
+
+	public static FDStatistic getStatistic() {
+		return new FDStatistic(START_TIME, ACTIVE_DESKTOPCOUNT.get(), TOTAL_SESSIONCOUNT.get(),
+				ACTIVE_SESSIONCOUNT.get(), TOTAL_DESKTOPCOUNT.get(), TOTAL_UPDATECOUNT.get(), ACTIVE_UPDATECOUNT.get());
+	}
+
+	private final static long START_TIME = System.currentTimeMillis();
+	private final static AtomicInteger TOTAL_DESKTOPCOUNT = new AtomicInteger(0);
+	private final static AtomicInteger TOTAL_SESSIONCOUNT = new AtomicInteger(0);
+	private final static AtomicInteger TOTAL_UPDATECOUNT = new AtomicInteger(0);
+	private final static AtomicInteger ACTIVE_SESSIONCOUNT = new AtomicInteger(0);
+	private final static AtomicInteger ACTIVE_DESKTOPCOUNT = new AtomicInteger(0);
+	private final static AtomicInteger ACTIVE_UPDATECOUNT = new AtomicInteger(0);
+
+	public void sessionCreated(Session sess) {
+
+		System.out.println("sessionCreated(...)");
+
+		ACTIVE_SESSIONCOUNT.incrementAndGet();
+		TOTAL_SESSIONCOUNT.incrementAndGet();
+	}
+
+	public void sessionDestroyed(Session sess) {
+
+		System.out.println("sessionDestroyed(...)");
+
+		ACTIVE_SESSIONCOUNT.decrementAndGet();
+	}
+
+	public void desktopCreated(Desktop desktop) {
+
+		System.out.println("desktopCreated(...)");
+
+		ACTIVE_DESKTOPCOUNT.incrementAndGet();
+		TOTAL_DESKTOPCOUNT.incrementAndGet();
+	}
+
+	public void desktopDestroyed(Desktop desktop) {
+
+		System.out.println("desktopDestroyed(...)");
+
+		ACTIVE_DESKTOPCOUNT.decrementAndGet();
+	}
+
+	public void beforeUpdate(Desktop desktop, @SuppressWarnings("rawtypes") List requests) {
+
+		System.out.println("beforeUpdate(...)");
+
+		ACTIVE_UPDATECOUNT.incrementAndGet();
+		TOTAL_UPDATECOUNT.incrementAndGet();
+	}
+
+	public void afterUpdate(Desktop desktop) {
+
+		System.out.println("afterUpdate(...)");
+
+		ACTIVE_UPDATECOUNT.decrementAndGet();
+	}
+}

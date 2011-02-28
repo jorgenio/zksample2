b5e01d8ba8d147ac2f693fb04bc9bc6418389514
terrytornado <terrytornado@44dfe7d0-046a-4be5-b101-54f5111bae67>
2011-02-28 15:19:26 +0000

    updated to zk 5.0.6 FL.20110223

--- zkoss/src/main/java/de/forsthaus/webui/index/IndexCtrl.java
+++ zkoss/src/main/java/de/forsthaus/webui/index/IndexCtrl.java
@@ -114,10 +114,10 @@ public class IndexCtrl extends GFCBaseCtrl implements Serializable {
 		 * month - the month (1 to 12).<br>
 		 * year - the year (1900 <= year <= 9999).<br>
 		 */
-		final Date date = new Day(22, 2, 2011).getStart();
+		final Date date = new Day(28, 2, 2011).getStart();
 
 		final String zkVersion = doGetZkVersion();
-		final String appVersion = this.appName + " v5.0.604 / " + ZksampleDateFormat.getDateFormater().format(date);
+		final String appVersion = this.appName + " v5.0.605 / " + ZksampleDateFormat.getDateFormater().format(date);
 
 		final String userName = ((UserImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
 		final String version = zkVersion + " | " + appVersion;
